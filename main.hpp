7c9f6446d0b5580fc19181b656e204e6fff3063e
wniazi5 <156951596+wniazi5@users.noreply.github.com>
2024-03-15 23:44:06 +0000

    2

--- main.hpp
+++ main.hpp
@@ -1,7 +1,24 @@
+#include <iostream> 
+using namespace std;
 
-//********************
-// Declare Function Prototypes
-//********************
+void getTwoValues(int begin, int end){
+    do {
+        cout << "Enter two integer values (begin < end): ";
+        cin >> begin >> end;
+    }
+    while ( begin >= end);
+}
+    int isPrime(int num) {
+        if (num <= 1) 
+        return 0 ;
+        if (num == 2) 
+        return 1;
+        if (num % 2 == 0)
+        return 0;
+    for (int i = 3; i * i <= num; i += 2){
+        if ( num)
+    }
+    }
 
 // ******************************
 // Implement all your functions here
